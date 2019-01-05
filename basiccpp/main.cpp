8842b6838ba390997ee54fafbbfbb4a462f91064
gunnerjay <gunner10@gmail.com>
2019-01-04 21:39:26 -0600

    intellisense off linux paths

--- basiccpp/main.cpp
+++ basiccpp/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <pthread.h>
 
-int main(void) {
-    int blah = 10000;
-    std::cout << "Hello world from c++ " << blah <<  " \n";
-    return 0;
+int main(void)
+{
+  int blah = 10000;
+  std::cout << "Hello world from c++ " << blah << " \n";
+  return 0;
 }
